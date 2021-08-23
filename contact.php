5d7b84bc74913da630b89404e303cc51e71241e3
Sam Valman <s.valman@hotmail.co.uk>
2021-08-23 10:57:30 +0100

    contact test

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("s.valman@hotmail.co.uk", "Stage Zero: Contact us",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
+
