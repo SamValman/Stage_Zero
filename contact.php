0de6cde28d50898cbd0e059fe2a1b1ef24feee6f
Sam Valman <s.valman@hotmail.co.uk>
2021-08-23 11:38:37 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,9 +14,19 @@ $_POST["insert your message here"]. "From: s.valman@hotmail.co.uk");
 
 
 ?>
-
-<!-- Sample HTML Form -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <!-- Sample HTML Form -->
 <form method="post" action="contact.php">
 <textarea name="message"></textarea>
 <input type="submit">
-</form>
+</form>  
+</body>
+</html>
