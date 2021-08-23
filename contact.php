cefc50793740cc03fc2c782d04f95ebba4ce27e3
Sam Valman <s.valman@hotmail.co.uk>
2021-08-23 11:34:37 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,10 +7,16 @@ if($_POST["message"]) {
 mail("s.valman@hotmail.co.uk", "Stage Zero: Contact us",
 
 
-$_POST["insert your message here"]. "From: an@email.address");
+$_POST["insert your message here"]. "From: s.valman@hotmail.co.uk");
 
 
 }
 
 
 ?>
+
+<!-- Sample HTML Form -->
+<form method="post" action="contact.php">
+<textarea name="message"></textarea>
+<input type="submit">
+</form>
